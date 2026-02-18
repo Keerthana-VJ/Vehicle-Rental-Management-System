f9efa350726988f85d6d8359d12144a7c6609968
Keerthana-VJ <jayanthivenkateswaran1973@gmail.com>
2026-02-18 19:37:19 +0530

    Success

---
+++ src/Model/Admin.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Admin {
+    private int admin_id;
+    private String admin_name;
+
+    public Admin(int admin_id,String admin_name)
+    {
+        this.admin_id=admin_id;
+        this.admin_name=admin_name;
+    }
+
+}
